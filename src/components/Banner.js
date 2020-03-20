c9530b6cd05feda78ed1017d6b0dbf66f1c132a9
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-03-20 20:56:19 -0300

    ajust mobile view

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,6 +16,34 @@ function Banner() {
                     <h1 className="titulo-amarelo"> 7 Medidas para Preservar Sua Empresa do COVID-19! </h1>
 
                     <p className="paragrafo"> Nesse E-book vamos compartilhar técnicas vencedoras que ajudaram <span className="span-bold"> GRANDES </span> empresários a passarem pelas mais duras crises!</p>
+                    <div className="section-two-mobile">
+
+                        <div className="retangulo">
+                            <h1 className="titulo-form"> Quero proteger meu negócio!</h1>
+
+                            <form method="POST" action="https://smtl.gama.academy/leads/4a6b6151-5ffc-11ea-92b8-cb0262e26cca">
+                                <p className="label-form"> Nome completo: </p>
+                                <input
+                                    name="name"
+                                    type="text"
+                                    pattern="^\D{2,}\s[\D\s]{2,}$"
+                                    title="Digite seu nome completo"
+                                    required
+                                />
+                                <p className="label-form"> Email: </p>
+                                <input
+                                    name="email"
+                                    type="email"
+                                    title="Digite um email válido"
+                                    required
+                                />
+                                <input name="redirectTo" type="hidden" value="https://gama.academy" />
+                                <button type="submit" name="meuEbook">QUERO MEU E-BOOK!</button>
+
+                            </form>
+                        </div>
+                    </div>
+
                     <p className="paragrafo">  <span className="span-bold">Baixe esse material e entenda: </span></p>
                     <div className="container-icons">
                         <div className="section-icon">
