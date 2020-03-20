51e12f10d6c24b30c4156f18d088fb8f2614cf2d
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-03-19 23:16:29 -0300

    quase concluido conforme mockup

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './style.css';
+import Pessoa from './assets/pessoas.png'; // Tell webpack this JS file uses this image
+import Money from './assets/money.png'; // Tell webpack this JS file uses this image
+import Joia from './assets/joia.png'; // Tell webpack this JS file uses this image
+
+
 
 
 function Banner() {
@@ -8,19 +13,58 @@ function Banner() {
         <div className="banner">
             <div className="container">
                 <div className="section-one">
-                    <h1 className="titulo-amarelo"> 7 Medidas para Preservar Sua Empresa do COVID-19 </h1>
+                    <h1 className="titulo-amarelo"> 7 Medidas para Preservar Sua Empresa do COVID-19! </h1>
 
                     <p className="paragrafo"> Nesse E-book vamos compartilhar técnicas vencedoras que ajudaram <span className="span-bold"> GRANDES </span> empresários a passarem pelas mais duras crises!</p>
                     <p className="paragrafo">  <span className="span-bold">Baixe esse material e entenda: </span></p>
+                    <div className="container">
+                        <div className="section-icon">
+                            <img src={Pessoa} alt="" />
+                            <p className="icon-title"> As medidas tomadas pelo governo</p>
+                        </div>
+                        <div className="section-icon">
+                            <img src={Money} alt="" />
+                            <p className="icon-title"> As medidas tomadas pelo governo</p>
+                        </div>
+                        <div className="section-icon">
+                            <img src={Joia} alt="" />
+                            <p className="icon-title"> Como não prejudicar seu negócio</p>
+                        </div>
+                    </div>
                 </div>
+
+
+
 
                 <div className="section-two">
                     <div className="retangulo">
                         <h1 className="titulo-form"> Quero proteger meu negócio!</h1>
+
+                        <form method="POST" action="URL do seu time">
+                            <p className="label-form"> Nome completo: </p>
+                            <input
+                                name="name"
+                                type="text"
+                                pattern="^\D{2,}\s[\D\s]{2,}$"
+                                title="Digite seu nome completo"
+                                required
+                            />
+                            <p className="label-form"> Email: </p>
+                            <input
+                                name="email"
+                                type="email"
+                                title="Digite um email válido"
+                                required
+                            />
+                            <input name="redirectTo" type="hidden" value="https://gama.academy" />
+                            <input name="debugMode" type="hidden" value="true" />
+                            <button type="submit" name="meuEbook">QUERO MEU E-BOOK!</button>
+
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './style.css';
 
+import Insta from './assets/insta-amarelo.svg'; // Tell webpack this JS file uses this image
+import Face from './assets/face-amarelo.svg'; // Tell webpack this JS file uses this image
+import Linked from './assets/linkedin-amarelo.svg'; // Tell webpack this JS file uses this image
+
+
 
 function Footer() {
 
     return (
 
-        <div className="container">
-            <h1> RODAPÉ BRABO</h1>
+        <div className="">
+            <div className="rodape">
+                <img src={Linked} alt="Linkedin" />
+                <img src={Face} alt="Facebook" />
+                <img src={Insta} alt="Instagram" />
+
+            </div>
         </div>
     )
 }
-
 export default Footer;
+
+
