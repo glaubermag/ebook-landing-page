f50f771289123da261d6b47af986b3095e905b56
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-03-21 00:05:56 -0300

    Updates

---
+++ precache-manifest.17e835a85e37d5e78177a130f7492148.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd9ca666a5823eedf625156f67a04bc",
+    "url": "/ebook-landing-page/index.html"
+  },
+  {
+    "revision": "c0166fdd5fef3a7fe4e1",
+    "url": "/ebook-landing-page/static/css/main.6f518588.chunk.css"
+  },
+  {
+    "revision": "27114727c6ab3b05e7c4",
+    "url": "/ebook-landing-page/static/js/2.4b78de69.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/ebook-landing-page/static/js/2.4b78de69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0166fdd5fef3a7fe4e1",
+    "url": "/ebook-landing-page/static/js/main.761552f1.chunk.js"
+  },
+  {
+    "revision": "f7151d3362841871abe0",
+    "url": "/ebook-landing-page/static/js/runtime-main.e66a5f9d.js"
+  },
+  {
+    "revision": "c2fe3e175490a303f87d53d4f970bd27",
+    "url": "/ebook-landing-page/static/media/face-amarelo.c2fe3e17.svg"
+  },
+  {
+    "revision": "ac5854015af16b110fab5b3772659dea",
+    "url": "/ebook-landing-page/static/media/fundo-ebook-lp.ac585401.png"
+  },
+  {
+    "revision": "c5b0e423f55e6e7d4adabd4feb1105b8",
+    "url": "/ebook-landing-page/static/media/insta-amarelo.c5b0e423.svg"
+  },
+  {
+    "revision": "fc7f636bd3c723e5138cd5e05f19ebc0",
+    "url": "/ebook-landing-page/static/media/joia.fc7f636b.png"
+  },
+  {
+    "revision": "77e1f85414eeb018f5c15e0a7df51415",
+    "url": "/ebook-landing-page/static/media/linkedin-amarelo.77e1f854.svg"
+  },
+  {
+    "revision": "1d3b9e34488cdb327950026fc0b6b0cb",
+    "url": "/ebook-landing-page/static/media/logo-pensando.1d3b9e34.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4fc9312e6f9cd7cdcb42ae7e746a342.js"
+  "/ebook-landing-page/precache-manifest.17e835a85e37d5e78177a130f7492148.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ebook-landing-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
